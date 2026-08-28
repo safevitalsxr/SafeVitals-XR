b8ba47fa897ddfeefe1075996b6441088e8d1714
karthikreddy331 <karthik.reddy@rapyder.com>
2026-08-28 13:24:43 +0530

    Change start command to run in development mode

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,9 +3,8 @@
 cd /home/ubuntu/SafeVitals-XR
 
 npm install
-
 npm run build
 
-pm2 restart safevitals-frontend || pm2 start npm --name safevitals-frontend -- start
+pm2 restart safevitals-frontend || pm2 start npm --name safevitals-frontend -- run dev
 
 pm2 save
